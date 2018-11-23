cddaca5f87f62d2724727f45c9f19cf6680c2712
vincentBaylly <vincent.baylly@computhand.com>
2018-11-22 22:06:44 -0500

    change default my-app value to vincent Web site

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyApp
+# Vincent Baylly Website Presentation
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-app'`, () => {
+  it(`should have as title 'presentation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('presentation');
   });
 
   it('should render title in a h1 tag', () => {
